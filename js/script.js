dfeb31c102e26e02c72fd6fa12f792dd8adbd50b
mike-zimmer <mzimmer94@gmail.com>
2022-07-06 15:21:18 -0700

    Completed assignment

---
+++ js/script.js
@@ -0,0 +1,123 @@
+// // STEP 1
+// let someMonth
+// let theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// // STEP 2
+// let numericExpression = 3 + 2
+// let stringExpression = 'Javascript is cool'
+// let x = 3
+// let y = 4
+// let booleanExpression = alert(x == y)
+// let nullExpression = null
+
+// // STEP 3
+// let complexExpression1 = 5 * 2 + (4 / 2)
+// let complexExpression2 = (10 - 4) + (100 / 2)
+
+// // STEP 4
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let zip
+// let code
+// let yourAge
+// let referralSource
+// let mayWeContactYou
+
+// // STEP 5
+// let yourAge
+// let lastName
+// let state
+// yourAge = 28
+// lastName = 'Zimmer'
+// state = 'California'
+
+// // STEP 6
+// let numberVar = 21
+// let stringVar = 'blackjack'
+// window.alert(numberVar + stringVar)
+
+// // STEP 7
+// let lName = 'Zimmer'
+// let age = 28
+// let booleanExample = (3 != 4)
+// let booleanString = lName + booleanExample
+// let booleanNum = age + booleanExample
+
+// // STEP 8
+// // This is not a valid string as there needs to be a backslash for the apostrophe used in "I'm"
+
+// // STEP 9
+// let z = null
+// console.log(z)
+// let x
+// console.log(x)
+
+// // STEP 10
+// let c = 'Javascript is cool'
+// console.log(typeof c)
+// let d = 21
+// console.log(typeof d)
+// let e = (3 != 4)
+// console.log(typeof e)
+// let f = {x:1}
+// console.log(typeof f)
+// let g
+// console.log(typeof g)
+
+// // STEP 11
+// window.alert('Hello ' + 'Mike Zimmer' + ', welcome to the JavaScript class!')
+
+// // STEP 12
+// let name = 'Mike Zimmer'
+// window.alert(`'Hello ${name}, welcome to the JavaScript class!'`)
+
+// // STEP 13
+// let course = 'JavaScript'
+// window.alert(`'Hello Mike Zimmer, welcome to the ${course} class!'`)
+
+// // STEP 14
+// window.alert('Hello ' + 'Mike Zimmer. \n' + 'Welcome to the JavaScript class!')
+
+// // STEP 15
+// fullName = prompt('Please enter your full name')
+// window.alert(`'Hello ${fullName}, welcome to the JavaScript class!'`)
+
+// // STEP 16
+// course = prompt('Please enter the course you\'re taking')
+// window.alert(`'Hello Mike Zimmer, welcome to the ${course} class!'`)
+
+// // STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// // STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+// // STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// // STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// // STEP 21
+// let y = 7
+// let z = 11
+// console.log(y == 7 || z == 11)
+
+// // STEP 22
+// let y = 7
+// let z = 11
+// console.log(y == 8 && z == 11)
